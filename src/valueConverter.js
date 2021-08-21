d72367454f3b98e90823127cec7524f31c179610
dales <dalesimmons0210@gmail.com>
2021-08-20 23:26:05 -0700

    working points calculator

--- src/valueConverter.js
+++ src/valueConverter.js
@@ -1,29 +1,46 @@
 export const valueConverter = (value) => {
     const points = [];
-    for (let i = 0; i < value.length; i++) {
+    for (let i = 0; i < value.length ; i++) {
+        if (value[i] === 'one') {
+            points.push(1);
+        } else if (value[i] === 'two') {
+            points.push(2);
+        } else if (value[i] === 'three') {
+            points.push(3);
+        } else if (value[i] === 'four') {
+            points.push(4);
+        } else if (value[i] === 'five') {
+            points.push(5);
+        } else {
+            points.push(6);
+            console.log("DEFAULT WAS PUSHED");
+        }
+        /*
         switch (value === true) {
-            case value[i] === 'one':
+            case (value[i] === 'one'):
+                console.log(value[i], ' was pushed')
                 points.push(1);
                 break;
-            case value[i] === 'two':
+            case (value[i] === 'two'):
                 points.push(2);
                 break;
-            case value[i] === 'three':
+            case (value[i] === 'three'):
                 points.push(3);
                 break;
-            case value[i] === 'four':
+            case (value[i] === 'four'):
                 points.push(4);
                 break;
-            case value[i] === 'five':
+            case (value[i] === 'five'):
                 points.push(5);
                 break;
-            case value[i] === 'six':
+            case (value[i] === 'six'):
                 points.push(6);
                 break;
             default:
                 break;
            
         }
+        */
     }
     
     return points;
